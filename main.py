5df2c407aa81e2fee6d435b64ec4139cdfb7112d
Anton Mozenbakh <anton.mozenbakh@student.tuke.sk>
2021-11-23 01:09:46 +0100

    +fill_vertical_or_horizontal_ship_counter() function to count number of ships per row/column for vertical/horizontal edges of maps (GUI)
    +updated version of Python (and syntax) to Python 3.10.0: using Python structural pattern matching  (similar to C switch case)
    
    +rewritten function draw_ships() using Python structural pattern
    
    +overall code improvements

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import json
 from enum import Enum
 import sys
 
+
 class Map_cell_state(Enum):
     EMPTY_STATE = 0
     LONELY_SHIP_STATE = 1
@@ -13,29 +14,29 @@ class Map_cell_state(Enum):
     VERTICAL_TOP_EDGE = 5
     VERTICAL_BOTTOM_EDGE = 6
 
+
 class Game_state(Enum):
     PLAYING = 0
     GAME_CLOSED = 1
     SOLVED = 2
     FAILED = 3
 
+
 WHITE = (255, 255, 255)
 BLACK = (0, 0, 0)
-RED = (255,   0,   0)
-BLUE = (  0,   0, 255)
+RED = (255, 0, 0)
+BLUE = (0, 0, 255)
 
 NUMBER_OF_CELLS = 10
 BLOCK_SIZE = 20
 LEFT_INDENT = 100
 UPPER_INDENT = 10
 
-MAP_NUMBER = str(2)
+MAP_NUMBER = str(1)
 
 our_first_map_file = open('maps.json', )
 
 all_maps_data = json.load(our_first_map_file)
-
-
 
 for k in range(1, 3):
     for x in range(0, NUMBER_OF_CELLS):
@@ -45,18 +46,38 @@ for k in range(1, 3):
 
     print('\n')
 
-
-
-
 # print(map_data["1"][0][8])
 
 ######
 
-def draw_ships():
-    x_block_center= LEFT_INDENT + BLOCK_SIZE / 2
-    y_block_center= UPPER_INDENT + BLOCK_SIZE / 2
-    circle_radius=8
+vertical_ship_counter = [0] * NUMBER_OF_CELLS
+horizontal_ship_counter = [0] * NUMBER_OF_CELLS
 
+def fill_vertical_or_horizontal_ship_counter(vertical):
+    for row in range(0, NUMBER_OF_CELLS):
+        for column in range(0, NUMBER_OF_CELLS):
+            match int(all_maps_data[MAP_NUMBER][row if vertical else column][column if vertical else row]):
+                case Map_cell_state.LONELY_SHIP_STATE.value | Map_cell_state.MIDDLE_SHIP_SECTION.value | \
+                     Map_cell_state.HORIZONTAL_LEFT_EDGE.value | Map_cell_state.HORIZONTAL_RIGHT_EDGE.value | \
+                     Map_cell_state.VERTICAL_TOP_EDGE.value | Map_cell_state.VERTICAL_BOTTOM_EDGE.value:
+                    if vertical:
+                        vertical_ship_counter[row] += 1
+                    else:
+                        horizontal_ship_counter[row] += 1
+
+                case Map_cell_state.EMPTY_STATE.value:
+                    pass
+
+                case _:
+                    sys.stderr.write('Problem in fill_vertical_or_horizontal_ship_counter(), ' +
+                                     'not declared new cell state with number:' +
+                                     str(all_maps_data[MAP_NUMBER][column][row]) + '\n')
+
+
+def draw_ships():
+    x_block_center = LEFT_INDENT + BLOCK_SIZE / 2
+    y_block_center = UPPER_INDENT + BLOCK_SIZE / 2
+    circle_radius = 8
 
     img_vertical_top_edge = pygame.transform.scale(pygame.image.load('assets/VERTICAL_TOP_EDGE.png'), (16, 16))
     img_vertical_bottom_edge = pygame.transform.scale(pygame.image.load('assets/VERTICAL_BOTTOM_EDGE.png'), (16, 16))
@@ -64,57 +85,36 @@ def draw_ships():
     img_horizontal_left_edge = pygame.transform.scale(pygame.image.load('assets/HORIZONTAL_LEFT_EDGE.png'), (16, 16))
     img_horizontal_right_edge = pygame.transform.scale(pygame.image.load('assets/HORIZONTAL_RIGHT_EDGE.png'), (16, 16))
 
-    for x  in range(0, NUMBER_OF_CELLS):
-        for y  in range(0, NUMBER_OF_CELLS):
+    for x in range(0, NUMBER_OF_CELLS):
+        for y in range(0, NUMBER_OF_CELLS):
 
-            if all_maps_data[MAP_NUMBER][y][x] == str(Map_cell_state.LONELY_SHIP_STATE.value):
-                pygame.draw.circle(screen, BLACK, (x_block_center, y_block_center), circle_radius)
-
-            elif all_maps_data[MAP_NUMBER][y][x] == str(Map_cell_state.MIDDLE_SHIP_SECTION.value):
-                pygame.draw.rect(screen, BLACK, (x_block_center - 7, y_block_center - 7, 15, 15))
-
-            elif all_maps_data[MAP_NUMBER][y][x] == str(Map_cell_state.VERTICAL_TOP_EDGE.value):
-                screen.blit(img_vertical_top_edge,(x_block_center-circle_radius, y_block_center-circle_radius ))
-
-            elif all_maps_data[MAP_NUMBER][y][x] == str(Map_cell_state.VERTICAL_BOTTOM_EDGE.value):
-                screen.blit(img_vertical_bottom_edge,(x_block_center-circle_radius, y_block_center-circle_radius ))
-
-            elif all_maps_data[MAP_NUMBER][y][x] == str(Map_cell_state.HORIZONTAL_LEFT_EDGE.value):
-                screen.blit(img_horizontal_left_edge, (x_block_center - circle_radius, y_block_center - circle_radius))
-
-            elif all_maps_data[MAP_NUMBER][y][x] == str(Map_cell_state.HORIZONTAL_RIGHT_EDGE.value):
-                screen.blit(img_horizontal_right_edge, (x_block_center - circle_radius, y_block_center - circle_radius))
-            elif all_maps_data[MAP_NUMBER][y][x] != str(Map_cell_state.EMPTY_STATE.value):
-                sys.stderr.write('Problem in draw_ships(), not declared new cell state!\n')
-                
-            y_block_center= y_block_center + BLOCK_SIZE
-
-        y_block_center= UPPER_INDENT + BLOCK_SIZE / 2
-        x_block_center= x_block_center + BLOCK_SIZE
+            match int(all_maps_data[MAP_NUMBER][y][x]):
+                case Map_cell_state.LONELY_SHIP_STATE.value:
+                    pygame.draw.circle(screen, BLACK, (x_block_center, y_block_center), circle_radius)
+                case Map_cell_state.MIDDLE_SHIP_SECTION.value:
+                    pygame.draw.rect(screen, BLACK, (int(x_block_center - 7), int(y_block_center - 7), 15, 15))
+                case Map_cell_state.VERTICAL_TOP_EDGE.value:
+                    screen.blit(img_vertical_top_edge, (x_block_center - circle_radius, y_block_center - circle_radius))
+                case Map_cell_state.VERTICAL_BOTTOM_EDGE.value:
+                    screen.blit(img_vertical_bottom_edge, (x_block_center - circle_radius, y_block_center - circle_radius))
+                case Map_cell_state.HORIZONTAL_LEFT_EDGE.value:
+                    screen.blit(img_horizontal_left_edge, (x_block_center - circle_radius, y_block_center - circle_radius))
+                case Map_cell_state.HORIZONTAL_RIGHT_EDGE.value:
+                    screen.blit(img_horizontal_right_edge, (x_block_center - circle_radius, y_block_center - circle_radius))
+                case Map_cell_state.EMPTY_STATE.value:
+                    pass
+                case _:
+                        sys.stderr.write('Problem in draw_ships(), ' +
+                        'not declared new cell state with number:' +
+                         str(all_maps_data[MAP_NUMBER][y][x]) + '\n')
+            y_block_center = y_block_center + BLOCK_SIZE
+        y_block_center = UPPER_INDENT + BLOCK_SIZE / 2
+        x_block_center = x_block_center + BLOCK_SIZE
     pygame.display.update()
 
 
-
-
-size = (LEFT_INDENT + 30 * BLOCK_SIZE, UPPER_INDENT + 15 * BLOCK_SIZE)
-
-pygame.init()
-
-screen = pygame.display.set_mode(size)
-pygame.display.set_caption("Lode")
-
-font_size = int(BLOCK_SIZE / 1.5)
-
-font = pygame.font.SysFont('arial', font_size)
-
-
-
-
-
-
 def draw_grid():
-    #letters = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J']
-    for i in range(NUMBER_OF_CELLS+1):
+    for i in range(NUMBER_OF_CELLS + 1):
         # horizontal grid1
         pygame.draw.line(screen, BLACK, (LEFT_INDENT, UPPER_INDENT + i * BLOCK_SIZE),
                          (LEFT_INDENT + NUMBER_OF_CELLS * BLOCK_SIZE, UPPER_INDENT + i * BLOCK_SIZE), 1)
@@ -135,30 +135,47 @@ def draw_grid():
                          (LEFT_INDENT + (i + 15) * BLOCK_SIZE, UPPER_INDENT + NUMBER_OF_CELLS * BLOCK_SIZE), 1)
 
         if i < NUMBER_OF_CELLS:
-            num = font.render(str(i + 1), True, BLACK)
-            #letters_hor = font.render(letters[i], True, BLACK)
+            num_vertical = font.render(str(vertical_ship_counter[i]), True, BLACK)
+            num_horizontal = font.render(str(horizontal_ship_counter[i]), True, BLACK)
 
-            num_width = num.get_width()
-            num_height = num.get_height()
-            #letters_hor_width = letters_hor.get_width()
+            # TODO: width and height for both num_vertical and num_horizontal
+            # width of rendered numbers
+            num_width = num_vertical.get_width()
+            # height of rendered numbers
+            num_height = num_vertical.get_height()
+            # letters_hor_width = letters_hor.get_width()
 
-            # vertical num grid1
-            screen.blit(num, (3.2 * LEFT_INDENT - (BLOCK_SIZE // 2 + num_width // 2),
-                              UPPER_INDENT + i * BLOCK_SIZE + (BLOCK_SIZE // 2 - num_height // 2)))
+            # vertical num_vertical grid1
+            screen.blit(num_vertical, (3.2 * LEFT_INDENT - (BLOCK_SIZE // 2 + num_width // 2),
+                                       UPPER_INDENT + i * BLOCK_SIZE + (BLOCK_SIZE // 2 - num_height // 2)))
             # horizontal letters grid1
-            screen.blit(num, (LEFT_INDENT + i * BLOCK_SIZE + (BLOCK_SIZE // 2 - num_width // 2), UPPER_INDENT + 10 * BLOCK_SIZE))
-            # vertical num grid2
-            screen.blit(num, (3.2 * LEFT_INDENT - (BLOCK_SIZE // 2 + num_width // 2) + 15 *
-                              BLOCK_SIZE, UPPER_INDENT + i * BLOCK_SIZE + (BLOCK_SIZE // 2 - num_height // 2)))
+            screen.blit(num_horizontal, (
+            LEFT_INDENT + i * BLOCK_SIZE + (BLOCK_SIZE // 2 - num_width // 2), UPPER_INDENT + 10 * BLOCK_SIZE))
+            # vertical num_vertical grid2
+            screen.blit(num_vertical, (3.2 * LEFT_INDENT - (BLOCK_SIZE // 2 + num_width // 2) + 15 *
+                                       BLOCK_SIZE, UPPER_INDENT + i * BLOCK_SIZE + (BLOCK_SIZE // 2 - num_height // 2)))
             # horizontal letters grid2
-            screen.blit(num, (LEFT_INDENT + i * BLOCK_SIZE + (BLOCK_SIZE // 2 - num_width // 2) + 15 * BLOCK_SIZE, UPPER_INDENT + 10 * BLOCK_SIZE))
+            screen.blit(num_horizontal, (
+            LEFT_INDENT + i * BLOCK_SIZE + (BLOCK_SIZE // 2 - num_width // 2) + 15 * BLOCK_SIZE,
+            UPPER_INDENT + 10 * BLOCK_SIZE))
+
+
+
+
+size = (LEFT_INDENT + 30 * BLOCK_SIZE, UPPER_INDENT + 15 * BLOCK_SIZE)
+pygame.init()
+screen = pygame.display.set_mode(size)
+pygame.display.set_caption("Lode")
+font_size = int(BLOCK_SIZE / 1.5)
+font = pygame.font.SysFont('arial', font_size)
 
 
 def main():
-
-    game_state=Game_state.PLAYING
+    game_state = Game_state.PLAYING
 
     screen.fill(WHITE)
+    fill_vertical_or_horizontal_ship_counter(vertical=False)
+    fill_vertical_or_horizontal_ship_counter(vertical=True)
     draw_grid()
     pygame.display.update()
 
@@ -171,8 +188,7 @@ def main():
     draw_ships()
     pygame.display.update()
 
-
-    while game_state==Game_state.PLAYING:
+    while game_state == Game_state.PLAYING:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 game_state = Game_state.GAME_CLOSED
@@ -180,6 +196,5 @@ def main():
 
 main()
 pygame.quit()
-
 
 our_first_map_file.close()
